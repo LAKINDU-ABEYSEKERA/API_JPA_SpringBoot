af71450da71e1c68111355cc9fd24134b49130b3
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-11-29 23:02:56 +0530

    Add Customer method implemented!

--- src/main/java/edu/icet/ecom/model/dto/CustomerDTO.java
+++ src/main/java/edu/icet/ecom/model/dto/CustomerDTO.java
@@ -1,2 +1,17 @@
+package edu.icet.ecom.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class CustomerDTO {
+
+    private String customerId;
+    private String name;
+    private String address;
+    private String contact;
 }
--- src/main/java/edu/icet/ecom/service/CustomerService.java
+++ src/main/java/edu/icet/ecom/service/CustomerService.java
@@ -1,2 +1,28 @@
+package edu.icet.ecom.service;
+
+import edu.icet.ecom.model.dto.CustomerDTO;
+import edu.icet.ecom.model.entity.Customer;
+import edu.icet.ecom.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public String addCustomer(CustomerDTO customerDTO) {
+        customerRepository.save(
+                new Customer(
+                        "C001",
+                        customerDTO.getName(),
+                        customerDTO.getAddress(),
+                        customerDTO.getContact()
+                ));
+
+        return "Customer Added Successfully";
+    }
+
+
 }
