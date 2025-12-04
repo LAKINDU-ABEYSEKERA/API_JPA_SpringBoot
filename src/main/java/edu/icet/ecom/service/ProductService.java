52a8b97d2409c59b9b7a7daea96e793a7b925b9e
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-12-04 22:07:55 +0530

    Product Add method Bug Fixed!..

--- src/main/java/edu/icet/ecom/service/ProductService.java
+++ src/main/java/edu/icet/ecom/service/ProductService.java
@@ -6,15 +6,29 @@ import edu.icet.ecom.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
 
+
+
     public String addProduct(ProductDTO productDTO) {
 
+        List<Product> productArray;
+        productArray = productRepository.findAll();
+
+        int nextId = 1;
+
+        for (Product array : productArray){
+            nextId++;
+        }
+
         Product product = new Product(
-                productDTO.getProductId(),
+                String.valueOf(nextId),
                 productDTO.getName(),
                 productDTO.getPrice()
 
